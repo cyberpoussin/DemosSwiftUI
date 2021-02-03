b4ca4e49c1041e56f263d882102023da5cdb597c
Adrien <adrnsmn@gmail.com>
2021-02-03 09:10:58 +0100

    Initial Commit

---
+++ NewFlowerApp/NewFlowerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewFlowerAppApp.swift
+//  NewFlowerApp
+//
+//  Created by Admin on 03/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct NewFlowerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
