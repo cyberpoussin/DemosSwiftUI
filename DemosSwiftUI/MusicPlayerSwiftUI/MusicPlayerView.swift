fc317780da8ae1a0c124fc4a392b270f2b933137
Adrien <adrnsmn@gmail.com>
2021-08-29 18:37:16 +0200

    Reorganization

--- DemosSwiftUI/MusicPlayerSwiftUI/MusicPlayerView.swift
+++ DemosSwiftUI/MusicPlayerSwiftUI/MusicPlayerView.swift
@@ -4,26 +4,21 @@
 //
 //  Created by Admin on 03/02/2021.
 //
-import AVFoundation
+
 import SwiftUI
 
 struct MusicPlayerView: View {
     @State private var played = false
     @State private var firstTime = true
-    @State private var audioPlayer: AVAudioPlayer?
 
     var body: some View {
         Button {
             played.toggle()
             if firstTime {
-                guard let dataAsset = NSDataAsset(name: "angele") else { return }
-                audioPlayer = try? perform(AVAudioPlayer(data: dataAsset.data, fileTypeHint: AVFileType.wav.rawValue),
-                                         orThrow: AudioLoaderError.loadPlayer)
-                audioPlayer?.play()
+                try? playFromAssets(filename: "angele")
                 firstTime = false
-                
             } else if !played {
-                audioPlayer?.stop()
+                audioPlayer?.pause()
             } else {
                 audioPlayer?.play()
             }
